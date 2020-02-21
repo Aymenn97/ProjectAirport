4e3100a20d4efb4682d45e436785c10c4ee060b7
Aymenn97 <aymen9339@hotmail.com>
2020-02-21 15:42:24 +0100

    initial project

---
+++ MyAirport.EF/Bagage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A.MyAirport.EF
+{
+    class Bagage
+    {
+        public int ID_BAGAGE { get; set; }
+        public int ID_VOL { get; set; }
+        public char CODE_IATA { get; set; }
+        public int DATA_CREATION { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ MyAirport.EF/MyAirportContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace A.MyAirport.EF
+{
+    class MyAirportContext : DbContext
+    {
+        public DbSet<Bagage> Bagages { get; set; }
+        public DbSet<Vol> Vols { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Blogging;Integrated Security=True");
+        }
+    }
+}
